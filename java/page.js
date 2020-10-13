e2d1c2c881a1da111514bc6124c4f9ac9717a5fc
Blake LaPoint <lapoinbe@clarkson.edu>
2020-10-13 08:35:21 -0400

    comments/fixes

--- java/page.js
+++ java/page.js
@@ -1,4 +1,4 @@
-var slideNum = i;
+var slideNum = 1;
 showSlide(slideNum);
 
 function moveSlides(n) {
@@ -13,13 +13,13 @@ function showSlide(n) {
   var i;
   var slide = document.getElementsByClassName("slides");
   var dots = document.getElementsByClassName("cursor");
-  var captionText = document.getElementsById("caption");
+  var captionText = document.getElementById("caption");
   if (n > slide.length) {slideNum = 1}
   if (n < 1) {slideNum = slide.length}
-  for (i=0; i<slide.length; i++) {
+  for (i=0; i < slide.length; i++) {
     slide[i].style.display = "none";
   }
-  for (i=0; i<dots.length; i++) {
+  for (i=0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace("active", "")
   }
   slide[slideNum-1].style.display="block";
